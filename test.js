ef5c553714cbe4ec36ae32396e21e902c1c43781
mohith140 <mohith.gilakathula@amista.com>
2025-06-30 11:05:58 +0000

    testiing

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
 // This is a dummy credential - SonarCloud should flag it as a security hotspot
-console.log(5/0)
-const password2 = "123456"; // hardcoded password
-console.log("User password is:", password2);
-const password3 = "123456"; // hardcoded password
-console.log("User password is:", password3);
-console.log(5/0)
+eval('5+6')
