a4767ac3062ce5488f45459d43d8521c07802d38
mohith140 <mohith.gilakathula@amista.com>
2025-07-29 05:46:26 +0000

    second3

--- srv/sql_inhection.js
+++ srv/sql_inhection.js
@@ -1,38 +1,3 @@
-const app = require('express');
-const mysql = require('mysql');
-
-app.get('/user', async (req, res) => {
-  const username = req.query.username; // ❌ user input directly used
-  const connection = mysql.createConnection({ /* connection config */ });
-  const API_KEY = "sk_live_123456789abcdef"; // ❗ GHAS will flag this
-  const secret="klkl"
-  console.log(API_KEY+" kll"+secret)
-  const id = req.data.id;
-  const db = await cds.connect.to('db');
-  const query = `SELECT * FROM Users WHERE ID = '${id}'`; // ❗ SQL Injection
-  return await db.run(query);
-  // const query = `SELECT * FROM users WHERE username = '${username}'`; // 🔥 SQL Injection
-  // cds.run(query);
-  // connection.query(query, (err, results) => {})
-  // connection.query(query, (err, results) => {
-  //     if (err) throw err;
-  //     res.send(results);
-  //   });
-//     if (err) throw err;
-//     res.send(results);
-//   });
-});
-
-app.listen(3000);
-this.on('getUser', async (req) => {
-  const API_KEY = "sk_live_123456789abcdef"; // ❗ GHAS will flag this
-  const secret="klkl"
-  console.log(API_KEY+" kll"+secret)
-  const id = req.data.id;
-  const db = await cds.connect.to('db');
-  const query = `SELECT * FROM Users WHERE ID = '${id}'`; // ❗ SQL Injection
-  return await db.run(query);
-});
 
 
 const { exec } = require('child_process');
@@ -50,22 +15,3 @@ app.get('/ping', async (req, res) => {
 
 app.listen(3001);
 
-
-const crypto = require('crypto');
-
-function hashPassword(pw) {
-  return crypto.createHash('md5').update(pw).digest('hex'); // ❌ MD5 is weak
-}
-
-
-const _ = require('lodash');
-const { cds2edm } = require('@sap/cds/libx/odata/utils');
-
-let userInput = {
-  "__proto__": {
-    "admin": true
-  }
-};
-
-let obj = {};
-_.merge(obj, userInput);  // ❌ Pollutes global Object prototype
