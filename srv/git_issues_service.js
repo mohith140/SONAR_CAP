f1e24a7c114b974822a14b3d33dcf73baa137a70
mohith140 <mohith.gilakathula@amista.com>
2025-09-02 13:58:38 +0000

    kl

--- srv/git_issues_service.js
+++ srv/git_issues_service.js
@@ -1,12 +1,5 @@
 const cds = require('@sap/cds');
-const fs = require('fs');
-const path = require('path');
-const https= require('https');
-const { ConnectivitySocks } = require('sap-cf-socks');
-const SMTPConnection = require("nodemailer/lib/smtp-connection");
 const app = require("express")()
-const pg = require("pg")
-const pool = new pg.Pool(config);
 module.exports =async function (srv) {
  const nodemailer = require("nodemailer");
  
@@ -16,13 +9,5 @@ module.exports =async function (srv) {
     req.params.category +
     "' ORDER BY PRICE";
  })
-   srv.on("updateEntry1",async (req)=> {
-     var query1 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
-    req.params.category +
-    "' ORDER BY PRICE";
-  pool.query(query1, [], function(err, results) {
-    // process results
-  });
- })
+
 }; 
